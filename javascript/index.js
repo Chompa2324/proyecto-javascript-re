573bc1b2b480cc18bd8eecec26eb95e06e367419
Francisco Oteo <panchyn.oteo@gmail.com>
2022-08-25 00:59:40 -0300

    agregando nueva pagina de css y creando boton de arriba en javascript

--- javascript/index.js
+++ javascript/index.js
@@ -61,3 +61,24 @@ fetch('./data.json')
     })
 
 
+//Boton que nos lleva hacia arriba-------------------------------------------------
+document.getElementById("button-up").addEventListener("click", scrollUp);
+
+function scrollUp(){
+    let currentScroll = document.documentElement.scrollTop;
+    if (currentScroll > 0){
+        
+        window.scrollTo (0, 0);
+    }
+}
+
+let buttonUp = document.getElementById("button-up");
+window.onscroll = function(){
+    let scroll = document.documentElement.scrollTop;
+    if (scroll > 200){
+        buttonUp.style.transform = "scale(1)";
+    }else if(scroll < 200){
+        buttonUp.style.transform = "scale(0)";
+    }
+}
+
